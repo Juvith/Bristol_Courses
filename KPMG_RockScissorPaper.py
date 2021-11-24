77de3a4b15d56620fa8c31d06c67143b70f93696
Juvith <juvithghosh@gmail.com>
2021-11-24 11:20:02 +0000

    Second Version

--- KPMG_RockScissorPaper.py
+++ KPMG_RockScissorPaper.py
@@ -1,36 +1,37 @@
 #KPMG Coding Challenge
 import random
-print("Welcome to Rock, Paper, Scissors")
-user_choice = input("What is your move? (rock, paper,scissors): ")
+print("Welcome to the Game of Rock, Paper, Scissors")
+#change in the user print statement and above print statement
+user_choice = input("What is your Selection? (Rock, Paper or Scissors): ")
 
-if user_choice == "paper" :
+if user_choice == "Paper" :
     print("You picked: " + user_choice)
-elif user_choice == "scissors" :
+elif user_choice == "Scissors" :
     print("You picked: " + user_choice)
 else :
     print("You picked: " + user_choice)
 
-computer_picked = random.choice(["rock","paper","scissors"])
+computer_picked = random.choice(["Rock","Paper","Scissors"])
 print("The computer picked: " + computer_picked)
 
-if computer_picked == "rock" :
-    if user_choice == "paper" :
+if computer_picked == "Rock" :
+    if user_choice == "Paper" :
         print("You Loose")
-    elif user_choice == "scissors" :
+    elif user_choice == "Scissors" :
         print("You Loose")
     else :
         print("Draw")
-elif computer_picked == "paper" :
-    if user_choice == "rock" :
+elif computer_picked == "Paper" :
+    if user_choice == "Rock" :
         print ("You Win")
-    elif user_choice == "scissors" :
+    elif user_choice == "Scissors" :
         print("You Win")
     else :
         print("Draw")
 else :
-    if user_choice == "rock" :
+    if user_choice == "Rock" :
         print("You Win")
-    elif user_choice == "paper" :
+    elif user_choice == "Paper" :
         print("You Loose")
     else :
         print("Draw")
